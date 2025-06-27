565a95653a2c176a577eb0029811a6baaccd1236
Vikas Gupta <146334720+vkg06@users.noreply.github.com>
2025-06-27 13:29:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.classList.add("active");
+}
+
+window.onload = function () {
+  document.querySelector(".tablinks").click(); // Click the first tab by default
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
+  });
+});
